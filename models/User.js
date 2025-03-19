279ce2cdc91b1a54f35ec44fcac108b9a59b452c
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-03-20 00:47:43 +0200

    Fix typo in UserSchema

--- models/User.js
+++ models/User.js
@@ -1,27 +1,30 @@
 const { required } = require("joi");
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please Provide name"],
-    minlength: 3,
-    maxlength: 50,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please Provide name"],
+      minlength: 3,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "Please Provide Email"],
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please Provide Valid email",
+      ],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please Provide Password"],
+      minlength: 8,
+      maxlength: 50,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Please Provide Email"],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please Provide Valid email",
-    ],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please Provide Password"],
-    minlength: 8,
-    maxlength: 50,
-  },
-}{timestamps: true});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("User", UserSchema)
+module.exports = mongoose.model("User", UserSchema);
