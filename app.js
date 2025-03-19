7af6870a791a9573428520d691aa4eeb548e7774
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-03-19 23:57:00 +0200

    Connect to MongDB

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 //connect DB
-
+const connectDB = require("./db/connect");
 //routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -31,8 +31,9 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is listening on http://localhost:${port}`)
     );
   } catch (error) {
     console.log(error);
